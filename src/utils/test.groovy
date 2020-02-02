f480ce68a6ce131d44626e20dd2f5321e6564d9e
Sun Gaofei <sungaofei@4paradigm.com>
2020-02-02 15:35:35 +0800

    push

--- src/utils/test.groovy
+++ src/utils/test.groovy
@@ -3,9 +3,7 @@ package utils
  * Created by sungaofei on 20/2/2.
  */
 
-class TestUtil{
 
-    def callstring(){
-        return "test gaofei"
-    }
+def callstring() {
+    return "test gaofei"
 }
--- vars/demo.groovy
+++ vars/demo.groovy
@@ -2,10 +2,7 @@
  * Created by sungaofei on 20/2/1.
  */
 
-import utils.TestUtil
-
 def call(){
-    def testUtile = new TestUtil()
     println(testUtile.callstring())
     println('This is test')
 }
