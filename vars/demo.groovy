7974b691c842325e358b3fbc192cf6fbaa97c474
Sun Gaofei <sungaofei@4paradigm.com>
2020-02-02 15:19:18 +0800

    push

--- vars/demo.groovy
+++ vars/demo.groovy
@@ -1,6 +1,11 @@
 /**
  * Created by sungaofei on 20/2/1.
  */
+
+import utils.TestUtil
+
 def call(){
+    def testUtile = new TestUtil()
+    println(testUtile.callstring())
     println('This is test')
 }
