0a0ac939b628352f6a1e6c6775d213a2343c747d
Pan Xin <panxin@4paradigm.com>
2020-02-06 22:56:03 +0000

    Update init_env.groovy

--- vars/init_env.groovy
+++ vars/init_env.groovy
@@ -1,14 +1,15 @@
 import utils.test
 
 def call(){
-
+     println("AA")
+     println('${DEPLOY_ENV')
      def remote = [:]
      remote.name = 'docker02'
      remote.host = '172.27.2.52'
      remote.user = 'gaofei'
      remote.port = 22
      remote.password = '1987720sgf'
-     remote.allowAnyHosts = true                                  
+     remote.allowAnyHosts = true   
      sshCommand remote: remote, command: "cd /opt/public/prophetee-deploy-autoui;cat 380cdhruntime.ini"
                     
 }
