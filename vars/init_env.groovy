3995a69bd42f5619d1a13d8bf1be1c8c6d3bbd2b
Pan Xin <panxin@4paradigm.com>
2020-02-06 22:58:23 +0000

    Update init_env.groovy

--- vars/init_env.groovy
+++ vars/init_env.groovy
@@ -2,7 +2,11 @@ import utils.test
 
 def call(){
      println("AA")
-     println('${DEPLOY_ENV')
+     println('${DEPLOY_ENV}')
+     println($DEPLOY_ENV) 
+     println(${DEPLOY_ENV}) 
+     println(DEPLOY_ENV)
+     
      def remote = [:]
      remote.name = 'docker02'
      remote.host = '172.27.2.52'
