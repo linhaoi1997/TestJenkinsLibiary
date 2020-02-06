b8be0d1909ce217790517590c08557afe5d457d3
Pan Xin <panxin@4paradigm.com>
2020-02-06 22:51:22 +0000

    Update demo.groovy

--- vars/demo.groovy
+++ vars/demo.groovy
@@ -7,14 +7,4 @@ import utils.test
 def call(){
     def test = new test()
     println(test.callstring())
-    
-    def remote = [:]
-    remote.name = 'docker02'
-    remote.host = '172.27.2.52'
-    remote.user = 'gaofei'
-    remote.port = 22
-    remote.password = '1987720sgf'
-    remote.allowAnyHosts = true                                  
-    sshCommand remote: remote, command: "cd /opt/public/prophetee-deploy-autoui;cat 380cdhruntime.ini"
-
 }
