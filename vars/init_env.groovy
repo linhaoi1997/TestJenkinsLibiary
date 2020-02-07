8d10480df67e181aea61ad1f7e7cbc41d1c62f7e
Pan Xin <panxin@4paradigm.com>
2020-02-07 01:45:09 +0000

    Update init_env.groovy

--- vars/init_env.groovy
+++ vars/init_env.groovy
@@ -7,7 +7,7 @@ def call(){
     remote.host = DEPLOY_ENV_HOST
     remote.user = DEPLOY_ENV_USER
     remote.password = DEPLOY_ENV_PASSWORD
-    remote.allowAnyHosts = DEPLOY_ENV_AllowAnyHosts  
+    remote.allowAnyHosts = DEPLOY_ENV_ALLOWANYHOSTS  
     sshCommand remote: remote, command: DEPLOY_ENV_COMMOND
 
 }
