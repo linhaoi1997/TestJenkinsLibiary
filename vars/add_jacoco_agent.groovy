fd3cad760d0bde9172b8258a5df32fd8bd5ca0fd
Pan Xin <panxin@4paradigm.com>
2020-02-07 01:16:42 +0000

    Add new file

---
+++ vars/add_jacoco_agent.groovy
@@ -0,0 +1,10 @@
+def call(){
+
+   def jacocoServer = [:]
+   jacocoServer.name = JACOCO_SERVER_NAME
+   jacocoServer.host = JACOCO_SERVER_HOST
+   jacocoServer.user = JACOCO_SERVER_USER
+   jacocoServer.password = JACOCO_SERVER_PASSWORD
+   jacocoServer.allowAnyHosts = JACOCO_SERVER_AllowAnyHosts
+   sshCommand remote: jacocoServer, command: JACOCO_SERVER_COMMOND
+}
