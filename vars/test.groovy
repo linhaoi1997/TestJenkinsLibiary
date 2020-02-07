f2271707727e56d9deff012c9fe4c56161279d03
Pan Xin <panxin@4paradigm.com>
2020-02-07 02:53:57 +0000

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -3,10 +3,9 @@ import utils.*
 def call(){
     sh """
     
-    SSHPASS=/usr/bin/sshpass 
     echo $JACOCO_SERVER_PASSWORD
     
-    pssh="$SSHPASS -p $JACOCO_SERVER_PASSWORD ssh -o StrictHostKeyChecking=no $JACOCO_SERVER_USER@$JACOCO_SERVER_HOST"
+    pssh="/usr/bin/sshpass -p $JACOCO_SERVER_PASSWORD ssh -o StrictHostKeyChecking=no $JACOCO_SERVER_USER@$JACOCO_SERVER_HOST"
     
     $pssh bash -s < jenkins-sharelib/tools/add_jacoco.sh
     
