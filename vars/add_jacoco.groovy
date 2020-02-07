cf63123bdc6f111a4a0cc59362ce998918a8baa0
Pan Xin <panxin@4paradigm.com>
2020-02-07 05:11:20 +0000

    Update add_jacoco.groovy

--- vars/add_jacoco.groovy
+++ vars/add_jacoco.groovy
@@ -4,7 +4,7 @@ def call(){
     echo "ADFSFSFSE"
     echo $APP_NAME
     echo $AGENT_INCLUDES
-    /usr/bin/sshpass -p $JACOCO_SERVER_PASSWORD ssh -o StrictHostKeyChecking=no $JACOCO_SERVER_USER@$JACOCO_SERVER_HOST bash -s < jenkins-sharelib/tools/add_jacoco.sh $NAMESPACES $DEPLOY_NAME  $AGENT_ENABLED $AGENT_INCLUDES $AGENT_PORT $AGENT_APPEND $APP_NAME
+    /usr/bin/sshpass -p $JACOCO_SERVER_PASSWORD ssh -o StrictHostKeyChecking=no $JACOCO_SERVER_USER@$JACOCO_SERVER_HOST bash -s < jenkins-sharelib/tools/add_jacoco.sh $NAMESPACES $DEPLOY_NAME  $AGENT_ENABLED $AGENT_PORT $AGENT_APPEND $APP_NAME
 
     """
 
