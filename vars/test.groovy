dcd14da80f0248ca35a380312ad0cb3608a92e0c
Pan Xin <panxin@4paradigm.com>
2020-02-07 01:36:15 +0000

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,15 +1,8 @@
 def call(){
      
-    println("testgroovy")
-    println(DEPLOY_ENV_NAME)
-    def remote = [:]
-    remote.name = DEPLOY_ENV_NAME
-    remote.host = DEPLOY_ENV_HOST
-    remote.user = DEPLOY_ENV_USER
-    remote.password = DEPLOY_ENV_PASSWORD
-    remote.allowAnyHosts = DEPLOY_ENV_AllowAnyHosts  
-    sshCommand remote: remote, command: DEPLOY_ENV_COMMOND
-      
+  sh """
+  sh ../tools/add_jacoco.sh
+  """
 
                     
 }
