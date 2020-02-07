b4429319a39ef3b216dcdafb353175d3da1d42b2
Pan Xin <panxin@4paradigm.com>
2020-02-07 03:36:28 +0000

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -6,7 +6,7 @@ def call(){
     echo $JACOCO_SERVER_PASSWORD
     echo "AABCC"
     
-    /usr/bin/sshpass -p $JACOCO_SERVER_PASSWORD ssh -o StrictHostKeyChecking=no $JACOCO_SERVER_USER@$JACOCO_SERVER_HOST bash -s < jenkins-sharelib/tools/add_jacoco.sh $JACOCO_SERVER_PASSWORD
-    
+    /usr/bin/sshpass -p $JACOCO_SERVER_PASSWORD ssh -o StrictHostKeyChecking=no $JACOCO_SERVER_USER@$JACOCO_SERVER_HOST bash -s < jenkins-sharelib/tools/add_jacoco.sh $NAMESPACES $DEPLOY_NAME $JACOCO_AGENT_ENABLED $JACOCO_AGENT_INCLUDES $JACOCO_AGENT_PORT $JACOCO_AGENT_APPEND $APP_NAME
+
     """
 }
