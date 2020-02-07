54bfda78ca5e41654407e8eda9d44a2f7b918e6f
Pan Xin <panxin@4paradigm.com>
2020-02-07 00:41:46 +0000

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -6,7 +6,6 @@ def call(){
     remote.name = DEPLOY_ENV_NAME
     remote.host = DEPLOY_ENV_HOST
     remote.user = DEPLOY_ENV_USER
-    remote.port = DEPLOY_ENV_PORT
     remote.password = DEPLOY_ENV_PASSWORD
     remote.allowAnyHosts = DEPLOY_ENV_AllowAnyHosts  
     sshCommand remote: remote, command: DEPLOY_ENV_COMMOND
