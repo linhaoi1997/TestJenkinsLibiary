397bd761340ea6e4cc3cb455da563101b34ae35b
Sun Gaofei <sungaofei@4paradigm.com>
2020-02-08 16:26:51 +0800

    push

--- vars/sendEmail.groovy
+++ vars/sendEmail.groovy
@@ -20,17 +20,17 @@ def call(){
 
     echo '111111111111'
 
-//    def http = new HTTPBuilder('http://auto.4paradigm.com')
-//    //根据responsedata中的Content-Type header，调用json解析器处理responsedata
-//    http.get(path:'/view/API/job/sage-sdk-test/45/allure/widgets/summary.json'){resp,json->
-//        println resp.status
-//
-//        println(json.statistic.passed)
-//
-//        json.each{
-//            println it
-//        }
-//    }
+    def http = new HTTPBuilder('http://auto.4paradigm.com')
+    //根据responsedata中的Content-Type header，调用json解析器处理responsedata
+    http.get(path:'/view/API/job/sage-sdk-test/45/allure/widgets/summary.json'){resp,json->
+        println resp.status
+
+        println(json.statistic.passed)
+
+        json.each{
+            println it
+        }
+    }
 //
 //    emailext body: """
 //    测试结束。 通过：${json.statistic.passed}
