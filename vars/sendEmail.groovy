dd65cbf1aca9e76515996962ce626ebf60d67f77
Sun Gaofei <sungaofei@4paradigm.com>
2020-02-08 16:14:28 +0800

    push

---
+++ vars/sendEmail.groovy
@@ -0,0 +1,40 @@
+/**
+ * Created by sungaofei on 20/2/8.
+ */
+@Grab(group = 'org.codehaus.groovy.modules.http-builder', module = 'http-builder', version = '0.7')
+
+import groovyx.net.http.HTTPBuilder
+
+
+
+import static groovyx.net.http.ContentType.*
+
+
+import static groovyx.net.http.Method.*
+
+
+
+
+
+def call(){
+    def http = new HTTPBuilder('http://auto.4paradigm.com')
+    //根据responsedata中的Content-Type header，调用json解析器处理responsedata
+    http.get(path:'/view/API/job/sage-sdk-test/45/allure/widgets/summary.json'){resp,json->
+        println resp.status
+
+        println(json.statistic.passed)
+
+        json.each{
+            println it
+        }
+    }
+
+    emailext body: """
+    测试结束。 通过：${json.statistic.passed}
+
+    """, subject: 'sage-sdk 测试结束', to: 'sungaofei@4paradigm.com'
+
+}
+
+
+
