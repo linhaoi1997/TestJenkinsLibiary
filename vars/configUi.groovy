5071846f6119f4568e209e290e2aabe2eab375ac
Pan Xin <panxin@4paradigm.com>
2020-02-11 07:47:05 +0000

    Update configUi.groovy

--- vars/configUi.groovy
+++ vars/configUi.groovy
@@ -4,33 +4,33 @@ def call(){
     
    cd UIAutomation/UIAutomation
    config=src/main/resources/config.properties
-   echo " " >> $config
+   echo " " >> src/main/resources/config.properties
    
    if [ -z $ENV_PORT ];then
-     echo "base.url=$HTTP_TYPE://$HOST" >> $config
+     echo "base.url=$HTTP_TYPE://$HOST" >> src/main/resources/config.properties
    else
-     echo "base.url=$HTTP_TYPE://$HOST:$ENV_PORT" >> $config
+     echo "base.url=$HTTP_TYPE://$HOST:$ENV_PORT" >> src/main/resources/config.properties
    fi
 
-   echo "browser.version=68.0.3440.84" >> $config
-   echo "remote=http://m7-prod-ssd001:30000/wd/hub" >> $config
-   echo "jdbc.url=jdbc:mysql://$HOST:$DB_PORT" >> $config
+   echo "browser.version=68.0.3440.84" >> src/main/resources/config.properties
+   echo "remote=http://m7-prod-ssd001:30000/wd/hub" >> src/main/resources/config.properties
+   echo "jdbc.url=jdbc:mysql://$HOST:$DB_PORT" >> src/main/resources/config.properties
 
    if [ -n "$ENV_WORKSPACE" ];then
-     echo "workspace=$ENV_WORKSPACE" >> $config
+     echo "workspace=$ENV_WORKSPACE" >> src/main/resources/config.properties
    fi
 
    if [[ $hive_Kerberos_open = CDHOPEN ]];then
 	  hdfs_url=""
    fi
-   echo "jenkins_workspace=$WORKSPACE" >> $config
+   echo "jenkins_workspace=$WORKSPACE" >> src/main/resources/config.properties
    echo "
    browser=$browser
    timeout=20000
    hdfs.nameNodeUrl=$hdfs_url
    hive_Kerberos_open=$hive_Kerberos_open
    isOpenLdb=$isOpenLdb
-   " >> $config
+   " >> src/main/resources/config.properties
 
    cat src/main/resources/config.properties
 
