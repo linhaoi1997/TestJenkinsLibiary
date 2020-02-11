f3334c44635a594ab4d0eb2d1b521ba7580a567f
Pan Xin <panxin@4paradigm.com>
2020-02-11 02:46:39 +0000

    Update sdp_sdk.sh

--- tools/sdp_sdk.sh
+++ tools/sdp_sdk.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
-rm -rf allure-report
-config=UIAutomation/src/main/resources/config.properties
+properties=src/main/resources/config.properties
+echo " 
+" >> $properties
+#db
+echo jdbc.url=jdbc:mysql://$host:$db_port >> $properties
+
+#workspace
+echo prophet.workspace=$sage_workspace >> $properties
+
+#accessKey
+echo prophet.accessKey=$accessKey  >> $properties
+
+if [ -z $env_port ];then
+    echo prophet.url=http://$host >> $properties
+else
+    echo prophet.url=https://$host:$env_port >> $properties
+fi
+
+
+#test environment
+
+cat src/main/resources/config.properties
