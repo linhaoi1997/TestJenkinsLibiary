7381bf4ae3adf676f4be28178a6e11f19cbbc910
Pan Xin <panxin@4paradigm.com>
2020-02-11 07:28:19 +0000

    Update configUI.groovy

--- vars/configUI.groovy
+++ vars/configUI.groovy
@@ -6,7 +6,6 @@ def call(){
     
    cd UIAutomation/UIAutomation
    config=src/main/resources/config.properties
-
    echo " " >> $config
    
    if [ -z $ENV_PORT ];then
@@ -16,14 +15,12 @@ def call(){
    fi
 
    echo "browser.version=68.0.3440.84" >> $config
-   
    echo "remote=http://m7-prod-ssd001:30000/wd/hub" >> $config
+   echo "jdbc.url=jdbc:mysql://$HOST:$DB_PORT" >> $config
 
    if [ -n "$ENV_WORKSPACE" ];then
      echo "workspace=$ENV_WORKSPACE" >> $config
    fi
-   
-   echo "jdbc.url=jdbc:mysql://$HOST:$DB_PORT" >> $config
 
    if [[ $hive_Kerberos_open = CDHOPEN ]];then
 	  hdfs_url=""
@@ -37,25 +34,25 @@ def call(){
    isOpenLdb=$isOpenLdb
    " >> $config
 
-cat src/main/resources/config.properties
+   cat src/main/resources/config.properties
 
 
-if [ $isOpenLdb = true ];then 	
-	sed -i '/<exclude name="dango.cases.sdp.sqlPreprocess\"\/>/d'  testsuite/sdp/sdp+modelCenter.xml
-fi
+   if [ $isOpenLdb = true ];then 	
+	sed -i '/dango.cases.sdp.sqlPreprocess/d'  testsuite/sdp/sdp+modelCenter.xml
+   fi
 
 if [ $hive_Kerberos_open = CDHOPEN ];then
-	sed -i '/<exclude name=\"dango.cases.sdp.hivecdhker\"\/>/d'  testsuite/sdp/sdp+modelCenter.xml
+	sed -i '/dango.cases.sdp.hivecdhker/d'  testsuite/sdp/sdp+modelCenter.xml
 elif [ $hive_Kerberos_open = CDHCLOSE ];then
-	sed -i '/<exclude name=\"dango.cases.sdp.hivecdh\"\/>/d'  testsuite/sdp/sdp+modelCenter.xml
+	sed -i '/dango.cases.sdp.hivecdh/d'  testsuite/sdp/sdp+modelCenter.xml
 elif [ $hive_Kerberos_open = C60OPEN ];then
-	sed -i '/<exclude name="dango.cases.sdp.hivec60\"\/>/d'  testsuite/sdp/sdp+modelCenter.xml
+	sed -i '/dango.cases.sdp.hivec60/d'  testsuite/sdp/sdp+modelCenter.xml
 elif [ $hive_Kerberos_open = C70OPEN ];then
-	sed -i '/<exclude name="dango.cases.sdp.hivec70\"\/>/d'  testsuite/sdp/sdp+modelCenter.xml
+	sed -i '/dango.cases.sdp.hivec70/d'  testsuite/sdp/sdp+modelCenter.xml
 elif [ $hive_Kerberos_open = HDP ];then
-	sed -i '/<exclude name="dango.cases.sdp.hivehdp\"\/>/d'  testsuite/sdp/sdp+modelCenter.xml
+	sed -i '/dango.cases.sdp.hivehdp/d'  testsuite/sdp/sdp+modelCenter.xml
 elif [ $hive_Kerberos_open = LEAP ];then
-	sed -i '/<exclude name="dango.cases.sdp.hiveleap\"\/>/d' testsuite/sdp/sdp+modelCenter.xml
+	sed -i '/dango.cases.sdp.hiveleap/d' testsuite/sdp/sdp+modelCenter.xml
 
 fi
 
