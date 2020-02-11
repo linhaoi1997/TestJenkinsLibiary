b451a60cdc3af57c721b8fad818c19639990d731
Pan Xin <panxin@4paradigm.com>
2020-02-11 03:42:20 +0000

    Update configSDK.groovy

--- vars/configSDK.groovy
+++ vars/configSDK.groovy
@@ -1,7 +1,9 @@
 def call(){
 
   sh """
- 
+    
+    echo "
+    " >> src/main/resources/config.properties
     echo jdbc.url=jdbc:mysql://$HOST:$DB_PORT >> src/main/resources/config.properties
     echo prophet.workspace=$WORKSPACE >> src/main/resources/config.properties
     echo prophet.accessKey=$ACCESSKEY  >> src/main/resources/config.properties
