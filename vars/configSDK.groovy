1744aa2cf7d18b74daf1bbba9f75849a41de6100
Pan Xin <panxin@4paradigm.com>
2020-02-11 03:47:26 +0000

    Update configSDK.groovy

--- vars/configSDK.groovy
+++ vars/configSDK.groovy
@@ -1,19 +1,20 @@
 def call(){
 
   sh """
+    cat ./src/main/resources/config.properties
     
-    echo ' ' >> src/main/resources/config.properties
-    echo jdbc.url=jdbc:mysql://$HOST:$DB_PORT >> src/main/resources/config.properties
-    echo prophet.workspace=$WORKSPACE >> src/main/resources/config.properties
-    echo prophet.accessKey=$ACCESSKEY  >> src/main/resources/config.properties
+    echo a >> ./src/main/resources/config.properties
+    echo jdbc.url=jdbc:mysql://$HOST:$DB_PORT >> ./src/main/resources/config.properties
+    echo prophet.workspace=$WORKSPACE >> ./src/main/resources/config.properties
+    echo prophet.accessKey=$ACCESSKEY  >> ./src/main/resources/config.properties
 
     if [ -z $ENV_PORT ];then
-       echo prophet.url=http://$HOST >> src/main/resources/config.properties
+       echo prophet.url=http://$HOST >> ./src/main/resources/config.properties
     else
-       echo prophet.url=https://$HOST:$ENV_PORT >> src/main/resources/config.properties
+       echo prophet.url=https://$HOST:$ENV_PORT >> ./src/main/resources/config.properties
     fi
 
-    cat src/main/resources/config.properties
+    cat ./src/main/resources/config.properties
 
     """
 }
