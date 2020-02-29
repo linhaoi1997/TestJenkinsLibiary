db716bfe78997eb310be65e4f19478fb622e56b0
Sun Gaofei <sungaofei@4paradigm.com>
2020-02-29 13:38:39 +0800

    push

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,9 +1,36 @@
-import utils.*
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
-def call(){
-    sh """
-    
-    /usr/bin/sshpass -p $JACOCO_SERVER_PASSWORD ssh -o StrictHostKeyChecking=no $JACOCO_SERVER_USER@$JACOCO_SERVER_HOST bash -s < jenkins-sharelib/tools/add_jacoco.sh $NAMESPACES $DEPLOY_NAME $JACOCO_AGENT_ENABLED $JACOCO_AGENT_INCLUDES $JACOCO_AGENT_PORT $JACOCO_AGENT_APPEND $APP_NAME
 
-    """
+//// How many threads to kick off
+//def nThreads = 3
+//def pool = Executors.newFixedThreadPool( nThreads )
+//
+//
+//
+//
+//pool.submit({
+//    sh "python3 -m pytest -m ${mark}"
+//})
+//
+//def timeout = 3
+//pool.shutdown()
+//println(33)
+//pool.awaitTermination( timeout, TimeUnit.SECONDS )
+//pool.shutdownNow()
+//println(44)
+
+
+def call(String[] marks, int timeout=60){
+    def pool = Executors.newFixedThreadPool( marks.size() )
+
+    for (int i = 0; i <= marks.size(); i ++){
+        pool.submit({
+            sh "python3 -m pytest -m ${mark}"
+        })
+    }
+    pool.shutdown()
+    pool.awaitTermination( timeout, TimeUnit.MINUTES )
+    pool.shutdownNow()
+
 }
