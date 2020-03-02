36421ba574992dc2ac2b5f58dec7a1b57e6659d2
Sun Gaofei <sungaofei@4paradigm.com>
2020-03-03 01:11:19 +0800

    push

--- vars/test.groovy
+++ vars/test.groovy
@@ -61,7 +61,7 @@ def call() {
     sh "ls -l ${System.getProperty("java.ext.dirs")}"
     this.class.classLoader.addURL(new URL("file://root/mysql-connector-java-8.0.13.jar"))
     Class.forName("com.mysql.jdbc.Driver", true, this.class.classLoader)
-    def sql = Sql.newInstance("jdbc:mysql://m7-qa-test03:3306/sage_sdk", "root", "root", "com.mysql.jdbc.Driver")
+    def sql = Sql.newInstance("jdbc:mysql://m7-qa-test03:3306/sage_sdk", "root", "root")
     query = "INSERT INTO func_test (name, version, total, passed, unknown, skipped, failed, broken, create_time) VALUES ('${JOB_NAME}', '${version}', " +
             "${total}, ${passed}, ${unknown}, ${skipped}, ${failed}, ${broken}, NOW())"
     sql.close()
