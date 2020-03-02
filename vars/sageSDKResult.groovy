76b1cd267528d54f5226b150081e52f53fb2e682
Sun Gaofei <sungaofei@4paradigm.com>
2020-03-02 18:31:36 +0800

    push

--- vars/sageSDKResult.groovy
+++ vars/sageSDKResult.groovy
@@ -16,18 +16,17 @@ import groovy.sql.Sql
 //可以指定maven仓库
 //@GrabResolver(name = 'aliyun', root = 'http://maven.aliyun.com/nexus/content/groups/public/')
 //加载数据库连接驱动包
-@Grab('mysql:mysql-connector-java:5.1.25')
-@GrabConfig(systemClassLoader=true)
+//@Grab('mysql:mysql-connector-java:5.1.25')
+//@GrabConfig(systemClassLoader=true)
 
 //global variable
 @Field jenkinsURL = "http://auto.4paradigm.com"
 
 
-
 def call(String version) {
-    ClassLoader classLoader = new GroovyClassLoader()
-    Map[] grapez = [[group : 'mysql', module : 'mysql-connector-java', version : '5.1.25']]
-    Grape.grab(classLoader: classLoader, grapez)
+//    ClassLoader classLoader = new GroovyClassLoader()
+//    Map[] grapez = [[group : 'mysql', module : 'mysql-connector-java', version : '5.1.25']]
+//    Grape.grab(classLoader: classLoader, grapez)
 
     def reportURL = ""
     if (env.BRANCH_NAME != "" && env.BRANCH_NAME != null) {
@@ -47,21 +46,27 @@ def call(String version) {
     //根据responsedata中的Content-Type header，调用json解析器处理responsedata
     http.get(path: "${reportURL}widgets/summary.json") { resp, json ->
         println resp.status
-        passed = Integer.parseInt((String)json.statistic.passed)
-        failed = Integer.parseInt((String)json.statistic.failed)
-        skipped = Integer.parseInt((String)json.statistic.skipped)
-        broken = Integer.parseInt((String)json.statistic.broken)
-        unknown = Integer.parseInt((String)json.statistic.unknown)
-        total = Integer.parseInt((String)json.statistic.total)
+        passed = Integer.parseInt((String) json.statistic.passed)
+        failed = Integer.parseInt((String) json.statistic.failed)
+        skipped = Integer.parseInt((String) json.statistic.skipped)
+        broken = Integer.parseInt((String) json.statistic.broken)
+        unknown = Integer.parseInt((String) json.statistic.unknown)
+        total = Integer.parseInt((String) json.statistic.total)
     }
     //创建sql实例
-    Class.forName("com.mysql.jdbc.Driver")
-    url = 'jdbc:mysql://m7-qa-test03:3306/sage_sdk?useUnicode=true&characterEncoding=utf8'
-    driver = 'com.mysql.jdbc.Driver'
-    username = 'root'
-    passwd = 'root'
-    def s = Sql.withInstance(url, username, passwd, driver) { sql ->
-        sql.execute "INSERT INTO func_test (name, version, total, passed, unknown, skipped, failed, broken, create_time) VALUES ('${JOB_NAME}', '${version}', " +
+//    Class.forName("com.mysql.jdbc.Driver")
+//    url = 'jdbc:mysql://m7-qa-test03:3306/sage_sdk?useUnicode=true&characterEncoding=utf8'
+//    driver = 'com.mysql.jdbc.Driver'
+//    username = 'root'
+//    passwd = 'root'
+    getDatabaseConnection(type: 'GLOBAL') {
+        def sqlString = "INSERT INTO func_test (name, version, total, passed, unknown, skipped, failed, broken, create_time) VALUES ('${JOB_NAME}', '${version}', " +
                 "${total}, ${passed}, ${unknown}, ${skipped}, ${failed}, ${broken}, NOW())"
+//        def params = ['test', 11]
+        sql sql: sqlString
     }
+//    def s = Sql.withInstance(url, username, passwd, driver) { sql ->
+//        sql.execute "INSERT INTO func_test (name, version, total, passed, unknown, skipped, failed, broken, create_time) VALUES ('${JOB_NAME}', '${version}', " +
+//                "${total}, ${passed}, ${unknown}, ${skipped}, ${failed}, ${broken}, NOW())"
+//    }
 }
