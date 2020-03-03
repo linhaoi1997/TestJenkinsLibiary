e4511c4af066b49ce4340851e21e7bacf31daf1b
Sun Gaofei <sungaofei@4paradigm.com>
2020-03-03 15:48:28 +0800

    push

--- vars/test.groovy
+++ vars/test.groovy
@@ -100,9 +100,9 @@ def call() {
 //        sql sql: sqlString
 //    }
 
-    map.each { feature, valueMap ->
 
-        getDatabaseConnection(type: 'GLOBAL') {
+    getDatabaseConnection(type: 'GLOBAL') {
+        map.each { feature, valueMap ->
             def sqlString = "INSERT INTO func_test (name, build_id, feature, version, total, passed, unknown, skipped, failed, broken, create_time) VALUES ('${JOB_NAME}', '${BUILD_ID}', '${feature}', '${version}', " +
                     "${valueMap['total']}, ${valueMap['passed']}, ${valueMap['unknown']}, ${valueMap['skipped']}, ${valueMap['failed']}, ${valueMap['broken']}, NOW())"
             sql sql: sqlString
