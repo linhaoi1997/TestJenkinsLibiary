a67cbdb966325731f317de920f51c654466963fe
Sun Gaofei <sungaofei@4paradigm.com>
2020-03-03 16:19:55 +0800

    push

--- vars/test.groovy
+++ vars/test.groovy
@@ -91,17 +91,16 @@ def getResultFromAllure() {
 def call() {
     def version = "release/3.8.2"
     getResultFromAllure()
-    def sqlString = ""
-    map.each { feature, valueMap ->
-        sqlString = "INSERT INTO func_test (name, build_id, feature, version, total, passed, unknown, skipped, failed, broken, create_time) VALUES ('${JOB_NAME}', '${BUILD_ID}', '${feature}', '${version}', " +
-                "${valueMap['total']}, ${valueMap['passed']}, ${valueMap['unknown']}, ${valueMap['skipped']}, ${valueMap['failed']}, ${valueMap['broken']}, NOW());" + sqlString
 
-
-    }
     println(sqlString)
     getDatabaseConnection(type: 'GLOBAL') {
+        map.each { feature, valueMap ->
+            def sqlString = "INSERT INTO func_test (name, build_id, feature, version, total, passed, unknown, skipped, failed, broken, create_time) VALUES ('${JOB_NAME}', '${BUILD_ID}', '${feature}', '${version}', " +
+                    "${valueMap['total']}, ${valueMap['passed']}, ${valueMap['unknown']}, ${valueMap['skipped']}, ${valueMap['failed']}, ${valueMap['broken']}, NOW())"
 
-        sql sql: sqlString
+            sql sql: sqlString
+        }
+
 
 //        valueMap.each { status, value ->
 //            getDatabaseConnection(type: 'GLOBAL') {
