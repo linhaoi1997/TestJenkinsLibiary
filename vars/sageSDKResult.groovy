2dcd99a9ec8d93cc9e440c536fad1bba30596251
Sun Gaofei <sungaofei@4paradigm.com>
2020-03-06 15:49:55 +0800

    push

--- vars/sageSDKResult.groovy
+++ vars/sageSDKResult.groovy
@@ -92,7 +92,7 @@ def getResultFromAllure() {
 def getCov(){
     def tagsoupParser = new org.ccil.cowan.tagsoup.Parser()
     def slurper = new XmlSlurper(tagsoupParser)
-    File file = new File("htmlcov/index.html")
+    File file = new File("sage-sdk-test/htmlcov/index.html")
     InputStream s = new FileInputStream(file)
     def htmlParser = slurper.parse(s)
 
