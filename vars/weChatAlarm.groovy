4065fccfb991feb9d2b18dcd73bffe4468eb4ca8
Pan Xin <panxin@4paradigm.com>
2020-05-09 03:09:36 +0000

    Update weChatAlarm.groovy

--- vars/weChatAlarm.groovy
+++ vars/weChatAlarm.groovy
@@ -1,6 +1,6 @@
 def call(){
 
-    sh """
+    sh '''
     curl -o temp.xml  "$JENKINS_URL/job/$JKS_JOB_NAME/$JKS_BUILD_NUMBER/api/xml"
     build_status=`grep 'result'  temp.xml`
     build_status=${build_status##*<result>}
@@ -15,7 +15,7 @@ def call(){
     HTTP_CODE=`curl -l -H $CONTEXT -X POST -d '{"msgtype": "markdown", "markdown": {"content": "<font color=\"info\">【'"$version"'自动化运行结果通知】</font>\n >运行结果：<font color=\"warning\">'"$build_status"'</font>\n >版本信息：'"$BRANCH"'\n >环境信息：'"$URL"'\n >[jenkins任务链接]('"$JENKINS_URL/job/$JKS_JOB_NAME/$JKS_BUILD_NUMBER"')\n >[allure报告链接]('"$JENKINS_URL/job/$JKS_JOB_NAME/$JKS_BUILD_NUMBER/allure/#/behaviors"')"}}' ${WEBHOOK_URL}`
    
    
-    """
+    '''
 
    
 }
