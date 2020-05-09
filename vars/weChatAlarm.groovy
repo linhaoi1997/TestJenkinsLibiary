6b1c6e1b5edb8a66ff5e3479ffd2a9054c1b2310
Pan Xin <panxin@4paradigm.com>
2020-05-09 03:31:49 +0000

    Update weChatAlarm.groovy

--- vars/weChatAlarm.groovy
+++ vars/weChatAlarm.groovy
@@ -14,8 +14,7 @@ def call(){
    
     '''
     sh """
-    version = "pipeline"
-    build_status = "SUCCESS"
+
     curl -l -H $CONTEXT -X POST -d '{"msgtype": "markdown", "markdown": {"content": "<font color=\"info\">【'"$version"'自动化运行结果通知】</font>\n >运行结果：<font color=\"warning\">'"$build_status"'</font>\n >版本信息：'"$BRANCH"'\n >环境信息：'"$URL"'\n >[jenkins任务链接]('"$JENKINS_URL/job/$JKS_JOB_NAME/$JKS_BUILD_NUMBER"')\n >[allure报告链接]('"$JENKINS_URL/job/$JKS_JOB_NAME/$JKS_BUILD_NUMBER/allure/#/behaviors"')"}}' ${WEBHOOK_URL}
    
     """
