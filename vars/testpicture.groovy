b5c5d58374dbe03d01647546113f07bd120433c4
Pan Xin <panxin@4paradigm.com>
2020-08-12 14:43:47 +0000

    Update testpicture.groovy

--- vars/testpicture.groovy
+++ vars/testpicture.groovy
@@ -4,21 +4,8 @@ import groovy.grape.Grape
 @Grab(group = 'org.seleniumhq.selenium', module = 'selenium-chrome-driver', version = '2.33.0')
 //@Grab("org.seleniumhq.selenium:selenium-firefox-driver:2.33.0")
 
-import org.openqa.selenium.By
-import org.openqa.selenium.WebDriver
-import org.openqa.selenium.chrome.ChromeDriver
-
-
-//set up the driver
- System.setProperty("webdriver.chrome.driver","./../tools/chromedriver.exe")
-
-//Define the driver
- def WebDriver driver = new ChromeDriver()
- driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS)
-
-//And now use this to visit Google
- def link = context.expand( 'www.baidu.com'); 
-
- driver.get(link)
-
- driver.quit()
+def call(String coverage = null, String version="release/3.8.2") {
+   sh '''
+   echo test
+   '''
+}
