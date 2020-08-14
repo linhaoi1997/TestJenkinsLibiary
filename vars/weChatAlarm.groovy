efccadc1dd7505441e69d58ce35b8f316b37fe09
Pan Xin <panxin@4paradigm.com>
2020-08-14 06:41:41 +0000

    Update weChatAlarm.groovy

--- vars/weChatAlarm.groovy
+++ vars/weChatAlarm.groovy
@@ -15,6 +15,7 @@ import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 import java.net.URL
 import java.net.URLConnection
+//import com.alibaba.fastjson.JSONObject;
 
 //可以指定maven仓库
 //@GrabResolver(name = 'aliyun', root = 'http://maven.aliyun.com/nexus/content/groups/public/')
@@ -41,11 +42,15 @@ def sendWechatAlarm() {
     } else {
         reportURL = "/view/API/job/${JOB_NAME}/${BUILD_NUMBER}/allure/"
     }
-
+    def s1=''
+    def pic1 = 'https://a1.qpic.cn/psc?/V50K8Aj22Pi7jG1cQHUv13mYFX1nzj4i/ruAMsa53pVQWN7FLK88i5tD45cdngcdQjsTSVi5*o5lEyjoe7mOItBetTiR97DTcNquldh*u9wz8kCo8hgP9NIufqR5EyOROUWzZNvoLrRE!/m&ek=1&kp=1&pt=0&bo=2wEJAQAAAAABF.I!&tl=3&vuin=1677684467&tm=1597251600&sce=60-3-3&rf=0-0'
+    def pic2 = 'http://a1.qpic.cn/psc?/V50K8Aj22Pi7jG1cQHUv13mYFX1nzj4i/ruAMsa53pVQWN7FLK88i5tHP1AzYWSQYCpP9GLLjmX2kVPpmgDpWYZyT7qEjJ9gca9K5NwjMAhXGJm7RwGD61afF2eRZuzBuLTCavKor4Pw!/m&ek=1&kp=1&pt=0&bo=2wEJAQAAAAABF.I!&tl=3&vuin=1677684467&tm=1597251600&sce=60-3-3&rf=0-0'
+    def pic = ''
+    def total=0
+    def passed=0
     //reportURL = "/view/API/job/${JOB_NAME}/${BUILD_NUMBER}/allure/"
     //reportURL = "/view/SDP/job/ui-auto-linkoop/9/allure/"
     HTTPBuilder http = new HTTPBuilder(jenkinsURL)
-    
     http.get(path: "${reportURL}widgets/summary.json") { resp, json ->
         println resp.status
         passed = Integer.parseInt((String) json.statistic.passed)
@@ -54,35 +59,56 @@ def sendWechatAlarm() {
         broken = Integer.parseInt((String) json.statistic.broken)
         unknown = Integer.parseInt((String) json.statistic.unknown)
         total = Integer.parseInt((String) json.statistic.total)
-        println passed
-        println total
+        s1="""Passed:${passed}  Failed:${failed}  Broken:${broken}\n环境信息 ${SGAE_URL}\n\n[查看测试报告]"""
+        
     }
-    
+    if(total==passed) { 
+            pic=pic1
+    } else{ 
+            pic=pic2
+    }
+
     // webURL="https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=c916b757-a1a2-416d-bf63-10fb8cf769e5"
     HTTPBuilder http1 = new HTTPBuilder("${WEBHOOK_URL}")
     
+    
+    String s="""
+    {
+    "msgtype": "news",
+    "news": {
+       "articles" : [
+           {
+               "title" : "${VERSION}自动化运行结果",
+               "description" : "${s1}",
+               "url" : "${JENKINS_URL}/job/${JOB_NAME}/${BUILD_NUMBER}/allure/#/behaviors",
+               "picurl" : "${pic}"
+           }
+           
+        ]
+    }
+   }
+   """
+    println s
+    def jsonSlurper = new groovy.json.JsonSlurper()
+    def object1 = jsonSlurper.parseText(s)
+    print object1
+    
     http1.request( POST, JSON ) { req ->
 	    
-	    String content="""<font color=\"info\">【${VERSION}自动化运行结果通知】</font>
-	    \n>环境信息：${SGAE_URL}
-	    >运行结果汇总如下：
-	    >total：${total}
-	    >passed：${passed}
-	    >failed：${failed}
-	    >skipped：${skipped}
-	    >broken：${broken}
-	    >unknown：${unknown}
-	    >[jenkins任务链接](${JENKINS_URL}/job/${JOB_NAME}/${BUILD_NUMBER})
-	    >[allure报告链接](${JENKINS_URL}/job/${JOB_NAME}/${BUILD_NUMBER}/allure/#/behaviors)
 	   
-	    """
-	    print content
-	    Map<String, String> bodyParam = new HashMap<>()
-	    bodyParam['content'] = content
-	    body = [
-	       msgtype : 'markdown',
-	       markdown : bodyParam
-	    ]
+	    String picurl="http://a1.qpic.cn/psc?/V50K8Aj22Pi7jG1cQHUv13mYFX1nzj4i/ruAMsa53pVQWN7FLK88i5tHP1AzYWSQYCpP9GLLjmX2kVPpmgDpWYZyT7qEjJ9gca9K5NwjMAhXGJm7RwGD61afF2eRZuzBuLTCavKor4Pw!/m&ek=1&kp=1&pt=0&bo=2wEJAQAAAAABF.I!&tl=3&vuin=1677684467&tm=1597251600&sce=60-3-3&rf=0-0"
+        Map<String, String> bodyParam = new HashMap<>()
+        
+	    bodyParam['picurl'] = picurl
+	    bodyParam['title'] = "测试title"
+        bodyParam['description'] = "测试description"
+    
+        Map<String, Map<String, String>> articles = new HashMap<>()
+        articles['articles']=bodyParam
+	    articles1 = [ articles ]
+	    
+	    body = object1
+	    
 	
 	response.success = { resp, json ->
 		// TODO process json data
@@ -116,5 +142,3 @@ def call(String coverage = null, String version="release/3.8.2") {
     //sendPostRequest("https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=c916b757-a1a2-416d-bf63-10fb8cf769e5", "msgtype=markdown&markdown={\"content\": \"test2\"}")
 
 }
-
-
