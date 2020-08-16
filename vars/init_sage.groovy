b4a554e0216c8f0d54d689bdeac931b268cc8198
Li Yuqing <liyuqing@4paradigm.com>
2020-08-16 21:23:53 +0800

    add: init sage lib

---
+++ vars/init_sage.groovy
@@ -0,0 +1,20 @@
+
+/**
+ * Created by liyuqing on 20/8/16.
+ */
+
+
+def call(targetHost,isHttps=false, sageVersion="4.0.0", hadoopUser="work", hadoopType="cdh"){
+    def run_status = sh (
+            script: """echo '123456' | ssh -t root@${targetHost} "wget http://pkg-plus.4paradigm.com/qa-tools/qa-scripts/init_sage_environment.sh && \
+                       sh -x init_sage_environment.sh ${isHttps} ${targetHost} ${sageVersion} ${hadoopUser} ${hadoopType}"
+                    """,
+            returnStatus:true
+    )
+    print run_status
+    // 失败退出
+    if (run_status != 0){
+        print '环境初始化执行失败，强制退出'
+        sh 'exit 1'
+    }
+}
