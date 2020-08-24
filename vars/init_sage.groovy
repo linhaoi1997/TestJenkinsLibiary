700c6e36b915922516ee1fd72e5debe87690a504
Li Yuqing <liyuqing@4paradigm.com>
2020-08-24 20:34:10 +0800

    fix init_sage

--- vars/init_sage.groovy
+++ vars/init_sage.groovy
@@ -7,7 +7,7 @@
 def call(targetHost,isHttps=false, sageVersion="4.0.0", hadoopUser="work", hadoopType="cdh"){
     public_key="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDAFrsiXGbTcdONpshr8iKqszKdKmhV/m6XkEGY2eCqLcmxsfOrLVgnV91r4bByRUg2nYp1/hSW6641AM6Yp1piN0p66AqymZgj95f/7vB1jXTLWlnvxUjMRkotqrFW297bb66/x9daNRSGW2TySexZ+j2zyegpZAlEMQUkPipimIfiKO4ybJzosJTex+GIKKbxgH3YQ7YYi3q7vBosfJByZOLx74VS8jkyNBgZkxmUZIG6TY1dcoumUoGV+nWzMJknEyDvXXG94U0C4mTiVXtMkWCqksf/bsttcVAh7wWQ5NdOaUy848ag7J41Ck3cHpQWHirf4M2qTYVaQhagGOtr root@qa-ops-platform"
     def copy_public_key = sh (
-            script: """cat ${public_key} > ~/.ssh/id_rsa.pub""",
+            script: """echo ${public_key} > ~/.ssh/id_rsa.pub""",
             returnStatus: true
     )
     print copy_public_key
