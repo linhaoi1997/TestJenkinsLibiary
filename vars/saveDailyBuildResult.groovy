328e7e39c5a99083aa8016b69c00fc93342c038c
Pan Xin <panxin@4paradigm.com>
2020-09-02 09:29:35 +0000

    Update saveDailyBuildResult.groovy

--- vars/saveDailyBuildResult.groovy
+++ vars/saveDailyBuildResult.groovy
@@ -90,13 +90,13 @@ def call() {
     getResultFromAllure()
 
     getDatabaseConnection(type: 'GLOBAL') {
-        echo 'database'
+        echo "database"
         //map.each { feature, valueMap ->
         //    def sqlString = "INSERT INTO func_test (name, build_id, feature, version, total, passed, unknown, skipped, failed, broken, create_time) VALUES ('${JOB_NAME}', '${BUILD_ID}', '${feature}', '${VERSION}', " +
         //            "${valueMap['total']}, ${valueMap['passed']}, ${valueMap['unknown']}, ${valueMap['skipped']}, ${valueMap['failed']}, ${valueMap['broken']}, NOW())"
         //    println(sqlString)
         //    sql sql: sqlString
-        }
+        //}
         //sql sql: sqlString
 
     }
