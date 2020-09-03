11d6316886caebb033fb8698dd505c8a7624130c
Sun Gaofei <sungaofei@4paradigm.com>
2020-09-03 11:27:21 +0800

    push

--- vars/saveReportToDB.groovy
+++ vars/saveReportToDB.groovy
@@ -121,13 +121,13 @@ def call(String coverage = null, String version="release/3.8.2") {
     getResultFromAllure()
 
     getDatabaseConnection(type: 'GLOBAL') {
-        map.each { feature, valueMap ->
-            def sqlString = "INSERT INTO func_test (name, build_id, feature, version, total, passed, unknown, skipped, failed, broken, create_time) VALUES ('${JOB_NAME}', '${BUILD_ID}', '${feature}', '${version}', " +
-                    "${valueMap['total']}, ${valueMap['passed']}, ${valueMap['unknown']}, ${valueMap['skipped']}, ${valueMap['failed']}, ${valueMap['broken']}, NOW())"
-            println(sqlString)
-
-            sql sql: sqlString
-        }
+//        map.each { feature, valueMap ->
+//            def sqlString = "INSERT INTO func_test (name, build_id, feature, version, total, passed, unknown, skipped, failed, broken, create_time) VALUES ('${JOB_NAME}', '${BUILD_ID}', '${feature}', '${version}', " +
+//                    "${valueMap['total']}, ${valueMap['passed']}, ${valueMap['unknown']}, ${valueMap['skipped']}, ${valueMap['failed']}, ${valueMap['broken']}, NOW())"
+//            println(sqlString)
+//
+//            sql sql: sqlString
+//        }
 
         def lineCov = 0
         def branchCov = 0
